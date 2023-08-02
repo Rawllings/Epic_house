60c7b148c970ad6d7c62ca96dd92282037c3a1ad
Rawllings Kiprop <rutojr01@gmail.com>
2023-08-02 11:32:49 +0300

    Created a react app using vite@latest

---
+++ house_hunt_app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-6xl text-center m-10 font-bold text-blue-900 ">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
